51f24f66e555931a2a6be343468706434336720e
Sarah Hart <sarah.hart.nc@gmail.com>
2022-03-04 13:59:11 -0500

    query for create operation

--- server.js
+++ server.js
@@ -35,9 +35,29 @@ app.listen(PORT, () => {
 // });
 
 // GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
     if (err) {
         console.log(err);
     }
-    console.log(row);
+    console.log(result);
 });
